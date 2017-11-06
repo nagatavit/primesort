041bc02d6f6a5b04d0f7584deec85df4b6cdb425
nagatavit <nagatavit@gmail.com>
2017-11-06 01:08:08 -0200

    Testes prontos e comentados

--- src/main.c
+++ src/main.c
@@ -1,17 +1,65 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+/* Vitor Nagata RA 178499 
+ *	This program receives a stream of numbers followed
+ * by a char '\n', and ends the sequency with a -1. The
+ * program then sorts the input in decreasing order and
+ * prints the non prime numbers received.
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int primo(int x){
+	
+	int aux=0;
+	
+	if (x == 0 || x == 1) {	// casos base (0 e 1 nao sao primos)
+		return (0);
+	}
+	
+	for(int i=1; i <= x/2; i++){	// varredura pelos divisores (seria necessario percorrer apenas ate raiz(x)
+		if (!(x%i)){	// eh divisor
+			aux++;
+		}
+		
+		if (aux > 1){
+			return (0);		// nao eh primo
+		}
+	}
+	
+	if (aux == 1){		// eh primo
+		return (1);
+	}
+	
+}
 
 int main() {
 
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+  int x = 0;
+  FILE *pipe_sort;
+  
+  if (( pipe_sort = popen("sort -r -n", "r")) == NULL){	// Verifica se o fork foi bem sucedido
+		perror("popen");
+		exit(1);
+	}
+	
+	while (1){
+		fscanf(pipe_sort,"%d\n", &x	);	// ordena entrada 
+		
+		if (x == -1){
+			break;
+		} else if (!(primo(x))){
+			printf("%d\n",x);		// imprime nao primos ja ordenados
+		}
+	
+	}
+	
+	pclose(pipe_sort);
+  
 }
+
+
+
+
+
+
+
